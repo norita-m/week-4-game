9ed006bf16e4e4d6395c3a4a0108b1246db30ea7
Nora Mata <norita@Noras-MacBook-Pro.local>
2016-11-16 17:28:50 -0600

    week 4 homework incomplete

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+/*Instructions: The computer randomnly chooses a number at the start of each new game
+There are four buttons on the page, each on is assigned a number that is hidden 
+The user tries to reach the random number w/o going over, by clicking on the crystals*/
+
+
+// Variables - buttons 
+
+// (Function) When page loads, (Function) have computer generate a number between 19 - 120 (class="number")
+
+// Assign each emoticon (button) with a number between 1-12. The number is hidden. 
+
+// Each time a button is clicked, update the score on the counter
+
+// If the player matches the random number, they win.
+
+// If the player goes over the random number, they lose. 
+
+// At the end of each game, reset the score counter. 
+
+// Keep the wins/losses. Do not reset this with each new game
+
+
+
